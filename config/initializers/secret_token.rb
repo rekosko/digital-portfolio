93cc47de8fb070e70bff0560971f509e303f8d84
ghoscik <rafal.ghost@gmail.com>
2011-01-12 12:45:53 +0100

    szablon projektu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalPortfolio::Application.config.secret_token = 'b6574aec5270b079b3618714aa724f5f65ccd6c8bdc8be58db89b2cbcc1dec00ed3f69e353357ead25d8e6d40c1bfb4d0c86280bf470d9ea901f7cc2c1c55095'
